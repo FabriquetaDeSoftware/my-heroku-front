0b7479d59851f50fd6cff369367c481e185fe1f6
cezarfbc <cezarfbc@gmail.com>
2025-07-07 16:15:20 -0300

    feat: Add SignInComponent with routing and initial template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainDashComponent } from './pages/main-dash/main-dash.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MainDashComponent,
+  },
+  {
+    path: 'sign-in',
+    component: SignInComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { MainDashComponent } from './pages/main-dash/main-dash.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
 
 @NgModule({
-  declarations: [AppComponent, SideBarComponent, MainDashComponent],
+  declarations: [AppComponent, SideBarComponent, MainDashComponent, SignInComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
